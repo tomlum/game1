c46f67458a1899d97d2fa1ab1732327ddeba9ba6
thlum <checkeredlines@gmail.com>
2014-09-24 16:28:54 -0400

    Added Control Mechanism
    The box displays the “Code” that needs to be entered before movement is
    unlocked.  You unlock the code with up down left or right and then move
    with wasd.
    
    The objective is to dodge objects (not implemented yet) while working
    through this hinderance.

--- Untitled5/src/untitled5/Ambi.java
+++ Untitled5/src/untitled5/Ambi.java
@@ -9,43 +9,101 @@ import java.awt.Color;
 import java.util.Random;
 public class Ambi {
         Posn center;
-	int radius;
-	IColor col;
+	int width = 150;
+        int height = 50;
+	IColor col = new Red();
+        String code = "up";
+        boolean locked;
 	
-	Ambi(Posn center, int radius, IColor col) {
+	Ambi(Posn center, String code, boolean locked) {
 		this.center = center;
-		this.radius = radius;
-		this.col = col;
+                this.code = code;
+                this.locked = locked;
+                    
 	}
+       
+        
+        public String newCode(){
+            locked = true;
+            int ran = Untitled5.randomInt(0,3);
+            if(ran == 0){
+                return "up";
+            }
+            else if(ran == 1){
+                return "left";
+            }
+            else if(ran == 2){
+                return "right";
+            }
+            else if(ran == 3){
+                return "down";
+            }
+            return "error";
+        }
+        
+        public void unlock(String ke){
+            if(this.code.equals(ke))
+                locked = false;
+        }
         
         WorldImage ambiImage(){
-            return new RectangleImage(this.center, this.radius*2, this.radius, this.col);
+            return new OverlayImages(
+                    new RectangleImage(this.center, this.width, this.height, this.col),
+                    ambiArrow());
+        }
+            
+        //returns the apropriate arrow image depending on code
+        WorldImage ambiArrow(){
+            if(code.equals("up")){
+                return new TextImage(this.center, "/\\",20, new Blue());
+            }
+            else if(code.equals("left")){
+                return new TextImage(this.center, "<-",20, new Blue());
+            }
+            else if(code.equals("right")){
+                return new TextImage(this.center, "->",20, new Blue());
+            }
+            else if(code.equals("down")){
+                return new TextImage(this.center, "\\/",20, new Blue());
+            }
+           return new TextImage(this.center, "error",20, new Blue());
+        }
+        
+        
+        WorldImage ambiText(){
+            return new TextImage(this.center, "->",5, new Blue());
         }
         
         //tie resetting to the movement keys vs the code keys
         
         public Ambi moveAmbi(String ke){
             
-            
-		if (ke.equals("right")){
+                unlock(ke);
+                if(!locked){
+		if (ke.equals("d")){
+                    newCode();
 			return new Ambi(new Posn(this.center.x + 50, this.center.y),
-							this.radius, this.col);
+							newCode(), true);
 		}
                 
-		else if (ke.equals("left")){
+		else if (ke.equals("a")){
+                    newCode();
 			return new Ambi(new Posn(this.center.x - 50, this.center.y),
-							this.radius, this.col);
+							newCode(), true);
 		}
-		else if (ke.equals("up")){
-			return new Ambi(new Posn(this.center.x, this.center.y - 50),
-							this.radius, this.col);
+		else if (ke.equals("w")){
+                    newCode();
+			return new Ambi(new Posn(this.center.x, this.center.y-50),
+							newCode(), true);
 		}
-		else if (ke.equals("down")){
-			return new Ambi(new Posn(this.center.x, this.center.y + 50),
-							this.radius, this.col);
+		else if (ke.equals("s")){
+                    newCode();
+			return new Ambi(new Posn(this.center.x, this.center.y+50),
+							newCode(), true);
 		}
 		
-		else
-			return this;
 	}
+                return this;
+                
+}
 }
--- Untitled5/src/untitled5/Untitled5.java
+++ Untitled5/src/untitled5/Untitled5.java
@@ -11,6 +11,11 @@ import java.util.Random;
 //until the game is more fully realized!
 
 public class Untitled5 extends World{
+    
+    static Random rand = new Random();
+    public static int randomInt( int min, int max ) {
+        return rand.nextInt((max - min) + 1) + min; }
+    
     Ambi Dexter;
     public WorldImage grid = new RectangleImage(new Posn(0, 0), 2000, 2000, new Black());
     
@@ -42,7 +47,7 @@ public class Untitled5 extends World{
     
 
     public static void main(String[] args) {
-        Untitled5 w = new Untitled5(new Ambi(new Posn(500, 500), 75, new Red()));
+        Untitled5 w = new Untitled5(new Ambi(new Posn(500, 500),"up", true));
         w.bigBang(1000, 1000, 0.3);
         
         
